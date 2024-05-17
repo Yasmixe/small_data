4582fae654083c42fd8cee0fd6b5ce247231ef16
Yasmine <88787992+Yasmixe@users.noreply.github.com>
2024-05-17 12:27:13 +0100

    Add files via upload

--- fichier.sql
+++ fichier.sql
@@ -2,4 +2,4 @@ create database small_data;
 
 create table small_data_url(url varchar(600), h1 varchar(100));
 
-insert into small_data_url(url, h1, occurence) values ('hey', 'h', 1);
+create table small_data_url2(url varchar(600), h1 varchar(100));
